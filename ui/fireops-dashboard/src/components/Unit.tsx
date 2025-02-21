507cc9dc62d660462cc54a9d0508d75a3c60ddf3
Lukas Neuhauser <neuhauser48@gmail.com>
2025-02-21 10:01:31 +0100

    Fixed graphical issue in Unit

--- ui/fireops-dashboard/src/components/Unit.tsx
+++ ui/fireops-dashboard/src/components/Unit.tsx
@@ -3,8 +3,8 @@ import { UnitState } from "../domain/UnitState";
 const Unit = ({unit}: {unit: UnitState}) => {
   return (
     <div className="h-24 w-48 flex m-4 text-white">
-      <div className="w-1/5 h-full text-white flex flex-wrap" style={{ backgroundColor: unit.unit_status_color ?? "gray" }}>
-        <span className="-rotate-90 translate-x-11">{unit.unit_status_id ? `(${unit.unit_status_id}) ` : "" }{unit.unit_status}</span>
+      <div className="w-1/5 h-full text-white flex" style={{ backgroundColor: unit.unit_status_color ?? "gray" }}>
+        <span className="-rotate-90 translate-x-8 text-nowrap">{unit.unit_status_id ? `(${unit.unit_status_id}) ` : "" }{unit.unit_status}</span>
       </div>
       <div className="w-4/5 h-full bg-red-600 flex flex-col flex-wrap items-center justify-center">
         <span className="text-2xl">{unit.unityp}</span>
